89b5d26bbd29b9b317e990046cc27d26ee30437b
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-04 19:45:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, { key: value})
+  return Object.assign({}, object, { key: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  
 }
