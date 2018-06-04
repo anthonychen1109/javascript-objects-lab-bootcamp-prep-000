849029e90bca989f9b7df3fd151e0448cae5183a
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-04 19:40:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = newObject();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { key: value})
+}
